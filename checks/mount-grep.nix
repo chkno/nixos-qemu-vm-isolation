0fb3685a199d2804993f5b414cf4cca1be35e66a
Scott Worley <scottworley@scottworley.com>
2021-11-20 17:38:13 -0800

    Note that useNixStoreImage exists now
    I'm disappointed that it doesn't work.  :(
    
    When it's fixed, it can replace this module.

--- checks/mount-grep.nix
+++ checks/mount-grep.nix
@@ -1,20 +1,31 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   name = "qemu-private-store-mount-grep";
 
   nodes = {
     shared = _: { };
     private = _: { imports = [ ../modules/qemu-vm-isolation.nix ]; };
+    useNixStoreImage = {
+      virtualisation = {
+        sharedDirectories = lib.mkForce { };
+        useNixStoreImage = true;
+      };
+    };
   };
 
   testScript = ''
     start_all()
     shared.wait_for_unit("multi-user.target")
     private.wait_for_unit("multi-user.target")
+    useNixStoreImage.wait_for_unit("multi-user.target")
 
     shared.succeed("[[ $(mount | grep -c virt) -gt 0 ]]")
     private.succeed("[[ $(mount | grep -c virt) -eq 0 ]]")
+    useNixStoreImage.succeed("[[ $(mount | grep -c virt) -eq 0 ]]")
 
     shared.succeed("[[ -e ${pkgs.pv} ]]")
     private.fail("[[ -e ${pkgs.pv} ]]")
+
+    # useNixStoreImage isn't ready until this works:
+    # useNixStoreImage.fail("[[ -e ${pkgs.pv} ]]")
   '';
 }
