2f94b5fbad026e89290b77cf291b2e611e145f98
Scott Worley <scottworley@scottworley.com>
2022-05-06 17:47:27 -0700

    Allow this to be used as a channel

---
+++ default.nix
@@ -0,0 +1,7 @@
+# This not an environment.
+#
+# This file exists to stop getAllExprs() in nix/src/nix-env/nix-env.cc
+# from recursing around in here and getting confused if this repo is
+# used as a channel.
+
+{ }
